2f59ca5430ede96b8364f835499a0ec56e3e50c7
Gouri <gouri7136@gmail.com>
2026-02-28 14:10:30 +0530

    audio play on clicking buttons with mouse

--- Basics/projects/drumkit/index.js
+++ Basics/projects/drumkit/index.js
@@ -5,9 +5,44 @@
 
 
 function dowhenclick (){
-  // alert("u clicked me")
-   console.log(this.innerHTML);
-  
+//   alert("u clicked me")
+   // console.log(this.innerHTML);
+   var buttonInnerHtml = this.innerHTML;
+   console.log(buttonInnerHtml)
+   switch(buttonInnerHtml){
+      case "w":
+         var audio = new Audio("audio/tom-1.mp3")
+         audio.play();
+         break;
+      case "a":
+         var audio = new Audio("audio/tom-2.mp3")
+         audio.play();
+         break;
+      case "s":
+         var audio = new Audio("audio/tom-3.mp3")
+         audio.play();
+         break;
+      case "d":
+         var audio = new Audio("audio/tom-4.mp3")
+         audio.play();
+         break;
+      case "j":
+         var audio = new Audio("audio/crash.mp3")
+         audio.play();
+         break;
+      case "k":
+         var audio = new Audio("audio/kick-bass.mp3")
+         audio.play();
+         break;
+      case "l":
+         var audio = new Audio("audio/snare.mp3")
+         audio.play();
+         break;
+
+      default:
+         console.log(buttonInnerHtml)
+
+   }
 }
 
 const numberofbuutons = document.querySelectorAll(".drum")
